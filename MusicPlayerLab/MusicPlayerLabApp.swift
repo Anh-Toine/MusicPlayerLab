05fa8dec808d78b85bd14566dc829eb4e29f7ab7
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-09-20 11:33:47 -0400

    Initial Commit

---
+++ MusicPlayerLab/MusicPlayerLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerLabApp.swift
+//  MusicPlayerLab
+//
+//  Created by Lukas Nguyen on 2022-09-20.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
